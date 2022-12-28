494a88a3839acdc13884a2927da2e64dcdff1f10
Aarv6546 <121565257+Aarv6546@users.noreply.github.com>
2022-12-28 21:21:08 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() 
+{
+    int a,b,c ;
+    printf("enter the value a \n");
+    scanf("%d",&a);
+    
+    printf("enter the valve b \n");
+    scanf("%d",&b);
+    
+    printf("enter the valve c \n");
+    
+    c=a;
+    a=b;
+    b=c;
+    
+    printf("Value of a = %d\n",a);
+    printf("Value of b = %d\n",b);
+    
+    return 0;
+    
+
+      
+    return 0;
+}
+
